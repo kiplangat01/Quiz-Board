f53b65fcd6ef3f1dff541f217c0a217c46405604
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-02-06 19:04:30 +0300

    first commit in this project

---
+++ index.js
@@ -0,0 +1,14 @@
+function check() {
+    var c=0
+var q1=document.test1.question1.value;
+var q2=document.test1.question2.value;
+var q3=document.test1.question3.value;
+var q4=document.test1.question4.value;
+var q5=document.test1.question5.value;
+var result=document.getElementById('afterclick');
+if  (q1=="script") {c++}
+if (q2=="script") {c++}
+if (q3=="let") {c++}
+if (q4=="for loop") {c++}
+if (q5=="if not") {c++}
+    test1.style.display="none";
